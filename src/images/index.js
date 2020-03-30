c2fd1ba0de41442168fa5fa633fab28491d51055
Isabella Tochterman <isabellatochterman@Isabellas-MacBook.local>
2020-03-30 00:12:58 -0400

    hover over feature

---
+++ src/images/index.js
@@ -0,0 +1,13 @@
+
+
+const josh1 = require("./assets/josh.jpg")
+const josh2 = require("./assets/josh2.jpg")
+const josh3 = require("./assets/josh3.jpg")
+const josh4 = require("./assets/josh4.jpg")
+const josh5 = require("./assets/josh3.jpg")
+const josh6 = require("./assets/josh4.jpg")
+
+export const arr = [josh1, josh2, josh3, josh4, josh5, josh6]
+ 
+
+
