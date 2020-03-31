cb1b6724fa5471611c3f7d3a33dd5497e4eba849
itochterman <irt221@nyu.edu>
2020-03-30 20:00:51 -0400

    update/modularize UI plus include some stray files.. sorry in advance to anyone who reads these which prob won't be anyone anyway

--- src/components/Poems.js
+++ src/components/Poems.js
@@ -1,17 +1,56 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import NavBar from './NavBar'
 import Header from './Header'
+import ChildPoems from './ChildPoems'
+import PoemDataService from '../services/poem.service'
+
 
 class Poems extends React.Component { 
-    render(){
-      return (
-        <div>
-            <Header title = "Poems"/>
-            <NavBar />
-            I'm poems!
-        </div>
-      )
-      }
+  constructor(){
+    super()
+    this.state = {
+    
+      all :  [{
+        id: "",
+        title: "",
+        description: "",
+        published: false
+      }],
+      isLoading: true
+
+    }
+  } 
+  async componentWillMount(){
+    await this.displayPoems();  
+  }
+
+  addPoem(){
+
+    PoemDataService.create({
+      title: this.state.title,
+      description: this.state.description
+      })
+  }
+  async displayPoems(){
+
+      const data = await PoemDataService.getAll().then((response) => {
+        return response.data
+      })
+      this.setState(
+      {all: data, isLoading: false}) 
+           
+  }
+      
+       
+  render(){
+    const sendProps = !this.state.isLoading
+    return (
+      <div>
+          <Header title = "Poems"/>
+          <NavBar />
+          <ChildPoems poems = {this.state.all} isLoading = {this.state.isLoading}/>
+      </div>
+    )
+    }
   }
   export default Poems 
