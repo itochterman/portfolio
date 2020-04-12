5d258db076d06e821864fa510c1bc5c6da213f9d
itochterman <irt221@nyu.edu>
2020-04-11 23:20:59 -0400

    changes to about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import Header from "./Header";
+import ios from "../images/assets/ios_project.jpg";
 
 class About extends React.Component {
   render() {
@@ -12,17 +13,24 @@ class About extends React.Component {
           <div className="column">
             <h2 className="aboutHeader">Education</h2>
             <ul>
+              <img className="columnImage" src={ios} />
+
               <li style={{ fontWeight: "bolder" }}> New York University</li>
             </ul>
           </div>
           <div className="column">
             <h2 className="aboutHeader"> Personal Projects</h2>
             <ul>
+              <img className="columnImage" src={ios} />
+
               <li style={{ fontWeight: "bolder" }}> New York University</li>
             </ul>
           </div>
           <div className="column">
             <h2 className="aboutHeader">Professional Experience</h2>
+            <ul>
+              <img className="columnImage" src={ios} />
+            </ul>
             <p>Some text..</p>
           </div>
         </div>
