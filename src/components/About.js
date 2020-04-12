62689d74e930d7980edda4b5b3ea45a6430dab02
itochterman <irt221@nyu.edu>
2020-04-12 15:53:50 -0400

    ui changes

--- src/components/About.js
+++ src/components/About.js
@@ -11,27 +11,71 @@ class About extends React.Component {
         <Header title="About Me" buttonBold="Home" />
         <div className="row">
           <div className="column">
-            <h2 className="aboutHeader">Education</h2>
             <ul>
-              <img className="columnImage" src={ios} />
-
-              <li style={{ fontWeight: "bolder" }}> New York University</li>
+              <div>
+                <img className="columnImage" src={ios} />
+              </div>
             </ul>
           </div>
           <div className="column">
-            <h2 className="aboutHeader"> Personal Projects</h2>
-            <ul>
-              <img className="columnImage" src={ios} />
+            <div className="aboutContainer">
+              <h2>Goals</h2>
+              <h3>~</h3>
+              <p1>
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt.
+              </p1>
 
-              <li style={{ fontWeight: "bolder" }}> New York University</li>
-            </ul>
-          </div>
-          <div className="column">
-            <h2 className="aboutHeader">Professional Experience</h2>
-            <ul>
-              <img className="columnImage" src={ios} />
-            </ul>
-            <p>Some text..</p>
+              <h2>Education</h2>
+              <h3>~</h3>
+
+              <p2>
+                {" "}
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt.
+              </p2>
+              <h2> Industry Experience</h2>
+              <h3>~</h3>
+
+              <p style={{ textAlign: "left" }}>
+                {" "}
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+                sit amet, consectetur, adipisci velit, sed quia non numquam eius
+                modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+                voluptatem. Ut enim ad minima veniam, quis nostrum
+                exercitationem ullam corporis suscipit laboriosam, nisi ut
+                aliquid ex ea commodi consequatur? Quis autem vel eum iure
+                reprehenderit qui in ea voluptate velit esse quam nihil
+                molestiae consequatur, vel illum qui dolorem eum fugiat quo
+                voluptas nulla pariatur?"
+              </p>
+              <h2> Personal Projects</h2>
+              <h3>~</h3>
+
+              <p style={{ textAlign: "left" }}>
+                "At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas molestias excepturi sint occaecati cupiditate
+                non provident, similique sunt in culpa qui officia deserunt
+                mollitia animi, id est laborum et dolorum fuga.
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,69 @@ import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import Header from "./Header";
+import axios from "axios";
 
 class Contact extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("body").value;
+    axios({
+      method: "POST",
+      url: "http://localhost:3001/send",
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        messsage: message,
+      },
+    }).then((response) => {
+      if (response.data.msg === "success") {
+        alert("Message Sent.");
+        document.getElementById("email-form").reset();
+      } else if (response.data.msg === "fail") {
+        alert("Message failed to send.");
+      }
+    });
+  }
   render() {
     return (
       <div>
         <Header title="Contact" />
-        I'm contact!
+        <form id="email-form" name="email-form" onSubmit={this.handleSubmit}>
+          <label for="firstName">First name: </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            defaultValue="First Name"
+          ></input>
+          <br />
+          <br />
+          <label for="lastName">Last name: </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            defaultValue="Last Name"
+          ></input>
+          <br />
+          <br />
+          <label for="email"> Email: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            defaultValue="Email"
+          ></input>
+          <br /> <br />
+          <textarea type="text" id="body" name="body"></textarea>
+          <br />
+          <br />
+          <input type="submit" value="Submit"></input>
+        </form>
       </div>
     );
   }
