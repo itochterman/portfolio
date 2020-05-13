13ffa691a8d48476c18ec3ac84cde5e7dcae673e
itochterman <irt221@nyu.edu>
2020-05-12 23:47:20 -0400

    env config stuff

--- src/http-header.js
+++ src/http-header.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://104.248.10.249:80/api",
+  // baseURL: "http://104.248.10.249:80/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_BASE,
+
   headers: {
     "Content-type": "application/json",
   },
